2bd1c816f89829450601eea5ec9f92ace4b72b15
Tércio Ribeiro <Trcio Ribeiro@Trcio.mshome.net>
2018-05-24 22:03:10 -0300

    Versao 1

---
+++ Quadrado/src/Principal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String args[]) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Quadrado quad = new Quadrado();
+		
+		
+		System.out.println("Digite o lado do quadrado: ");
+		quad.lado = scan.nextFloat();	
+		
+		
+		
+		
+	}
+
+}
---
+++ Quadrado/src/Quadrado.java
@@ -0,0 +1,12 @@
+
+public class Quadrado {
+	
+	float lado;
+	float area;
+	
+	public float calc_area(float x) {
+		
+		return x * x;
+	}
+
+}
